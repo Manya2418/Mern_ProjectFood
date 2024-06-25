05fcf7196fc65121d5a88ffce8e183e1f8fe9f5c
Manya2418 <manyasahu94@gmail.com>
2024-06-25 23:30:07 +0530

    update

--- backend/DbConnection/DbConnect.js
+++ backend/DbConnection/DbConnect.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
-const dbconnection=mongoose.connect('mongodb://localhost:27017',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
+const dbconnection=mongoose.connect('mongodb+srv://manyasahu:Manya123@cluster0.zgfhul6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
     console.log("Connected")
 }).catch("not connected")
 
 export default dbconnection;
+
+
+// mongodb://localhost:27017
+
+// mongodb+srv://manyasahu:Manya123@cluster0.zgfhul6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ import cartRouter from './Routes/cartRoutes.js';
 import orderRouter from './Routes/orderRoute.js';
 import adminRouter from './Routes/AdminRoute.js'
 import dotenv from 'dotenv'
-import Stripe from 'stripe';
 const app=express();
 app.use(bodyParser.json())
 const port=5000;
@@ -17,7 +16,6 @@ dotenv.config();
 app.use(cors());
 app.use(express.json())
 dbconnection;
-const stripe=Stripe("sk_test_51NmBxVSJm0EOvE96GfRtoViGGWb838JUFEP8lJgK2ekvKVL496R9kwU9m8wlh08kpI7WLtGHnXlknboJ8SjwVOqW00j5qYKm7G")
 app.use('/restaurant',restaurantRouter)
 app.use('/menus',menuRouter)
 app.use('/user',userRouter);
@@ -25,22 +23,7 @@ app.use('/cart',cartRouter)
 app.use('/order',orderRouter)
 app.use('/admin',adminRouter) 
 
-app.post('/payment/charge', async (req, res) => {
-  const { amount, paymentMethodId } = req.body;
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: 'inr',
-      payment_method: paymentMethodId,
-      confirm: true,
-    });
-    console.log('Payment Intent:', paymentIntent);
-    res.status(200).json({ message: 'Payment successful!', paymentIntent });
-  } catch (error) {
-    console.error('Error processing payment:', error);
-    res.status(500).json({ error: error.message });
-  }
-});
+
 app.listen(port, (req,res) => {
     console.log(`Server running at http://localhost:${port}/`);
   });
